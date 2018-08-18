e17bf2f20137abe5b706bbf97743cda6e7d81e6a
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-18 18:10:39 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,9 @@
 class Triangle
   # write code here
+  attr_accessor :kind, :side_1, :side_2, :side_3
   def initialize(side_1, side_2, side_3)
-
+    @side_1=side_1
+    @side_2=side_2
+    @side_3=side_3
   end
 end
