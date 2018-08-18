27a0bb5694a16757007b2146cbbff29e02e3aa66
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-18 18:14:22 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,12 +8,16 @@ class Triangle
   end
 
   def kind
-
+    if side_1==side_2 && side_1==side_3
+      :equilateral
+    elsif side_1==side_2 || side_1==side_3 || side_2==side_3
+      :isosceles
+    else
+      :scalene
+    end
   end
 
   class TriangleError < StandardError
 
   end
 end
-
-
